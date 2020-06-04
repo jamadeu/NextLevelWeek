f4e5a5e28374d28c16f2deecb6f0376956334045
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-04 09:53:33 -0300

    Day 3

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -14,8 +14,8 @@ const populateUfs = () => {
 populateUfs();
 
 const getCities = (event) => {
-  const citySelect = document.querySelector("select[name=city]")
-  const stateInput = document.querySelector("select[name=state]")
+  const citySelect = document.querySelector("[name=city]")
+  const stateInput = document.querySelector("[name=state]")
 
 
   const UF = event.target.value
@@ -24,11 +24,14 @@ const getCities = (event) => {
   stateInput.value = event.target.options[indexOfSelectedState].text
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UF}/municipios`
+  citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+  citySelect.disabled = true
+  
   fetch(url)
     .then((res) => { return res.json()})
     .then((cities) => {
       for(city of cities){
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
       }
 
       citySelect.disabled = false
@@ -40,5 +43,33 @@ document
   .addEventListener("change", getCities);
 
 
+  //Itens de coleta
+  const itemsToCollect = document.querySelectorAll(".items-grid li")
 
+  for (item of itemsToCollect){
+    item.addEventListener("click", handleSelectItem)
+  }
+
+  const colectedItems = document.querySelector("input[name=items]")  
+
+  let selectedItems = []
+
+  function handleSelectItem(event){
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex(item => item === itemId
+    )
+
+    if(alreadySelected >= 0){
+      const filteredItems = selectedItems.filter(item => item !== itemId)
+      selectedItems = filteredItems
+    }else{
+      selectedItems.push(itemId)
+    }
+
+    colectedItems.value = selectedItems
+  }
 
