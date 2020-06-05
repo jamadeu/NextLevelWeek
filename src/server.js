470da48a4d0716e8d8311fe87e06e2da710bdcfd
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-05 11:44:37 -0300

    Day 5

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const server = express()
 
+const db = require('./database/db')
+
 server.use(express.static("public"))
+server.use(express.urlencoded({ extended: true }))
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -14,11 +17,62 @@ server.get('/', (req, res) => {
 })
 
 server.get('/create-point', (req, res) => {
+
   return res.render("create-point.html")
 })
 
+server.post('/savepoint', (req, res) => {
+
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+      ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+      req.body.image,
+      req.body.name,
+      req.body.address,
+      req.body.address2,
+      req.body.state,
+      req.body.city,
+      req.body.items
+    ]
+
+  function afterInsertData(err){
+    if(err){
+      return console.log(err)
+    }
+
+    console.log("Cadastrado")
+    console.log(this)
+  }
+
+  db.run(query, values, afterInsertData)
+  return res.render("create-point.html", { saved: true })
+})
+
 server.get('/search', (req, res) => {
-  return res.render("search-results.html")
+  const seatch = req.query.search
+
+  if(seatch === ""){
+    return res.render("search-results.html", { total: 0 })
+  }
+
+  db.all(`SELECT * FROM places WHERE city LIKE '%${seatch}%'`, function(err, rows) {
+    if(err){
+      return console.log(err)
+    }
+
+    const total = rows.length
+    return res.render("search-results.html", {places: rows, total })
+  })
 })
 
 server.listen(3000)
